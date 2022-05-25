e41e11b44911da5ff27162c0122a667233b11df0
Randall <randallnhr@gmail.com>
2022-05-25 22:49:22 +0800

    CSS Update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <div className="ScheDUE">hello</div>;
+  return <div className="ScheDUE">
+    <Header />
+  </div>;
 }
 
 export default App;
